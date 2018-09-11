cb2e31f0e4debebb645489641ac63dd473ec38ba
Henry Winckles <rc0032427@student.riversidecollege.ac.uk>
2018-09-11 10:42:04 +0100

    not moving

--- move picture/Form1.Designer.cs
+++ move picture/Form1.Designer.cs
@@ -37,7 +37,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(401, 27);
+            this.button1.Location = new System.Drawing.Point(338, 33);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 23);
             this.button1.TabIndex = 2;
